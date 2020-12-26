932592fcd3e4599a461bcd5bdf0d38c8ebae86e1
Gabriel Frassl <gabriel.frassl@stud.fh-campuswien.ac.at>
2020-12-26 14:29:42 +0100

    Initial commit

---
+++ src/main/java/at/ac/campuswien/fh/foodsy/foodsy_backend/FoodsyBackendApplication.java
@@ -0,0 +1,14 @@
+package at.ac.campuswien.fh.foodsy.foodsy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsyBackendApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/at/ac/campuswien/fh/foodsy/foodsy_backend/FoodsyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.campuswien.fh.foodsy.foodsy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
